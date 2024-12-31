17d2c6df2be7773c7d5a9b0de86ba3db1860ee21
Aymanmahameed <aimanmhamid1234@gmail.com>
2024-12-31 15:03:39 +0200

    lecturer are the best

--- ManageParty/src/boundary/Main.java
+++ ManageParty/src/boundary/Main.java
@@ -3,7 +3,7 @@ package boundary;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello every body!!!!!!!");
+		System.out.println("Generative Ai course lecturer are the best!!");
 		new Login();
 		
 	}
